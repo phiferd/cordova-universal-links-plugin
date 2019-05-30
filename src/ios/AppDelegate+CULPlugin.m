57ffa26e867e025ed387ec7a9beece1e14eddce4
phiferd <dan.phifer@gmail.com>
2019-05-30 22:27:44 +0800

    Delegating all work to Branch.
    Firebase requires this particular plugin, but we want Branch to do all the work

--- src/ios/AppDelegate+CULPlugin.m
+++ src/ios/AppDelegate+CULPlugin.m
@@ -6,6 +6,7 @@
 
 #import "AppDelegate+CULPlugin.h"
 #import "CULPlugin.h"
+#import "Branch.h"
 
 /**
  *  Plugin name in config.xml
@@ -15,18 +16,8 @@ static NSString *const PLUGIN_NAME = @"UniversalLinks";
 @implementation AppDelegate (CULPlugin)
 
 - (BOOL)application:(UIApplication *)application continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
-    // ignore activities that are not for Universal Links
-    if (![userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb] || userActivity.webpageURL == nil) {
-        return NO;
-    }
-    
-    // get instance of the plugin and let it handle the userActivity object
-    CULPlugin *plugin = [self.viewController getCommandInstance:PLUGIN_NAME];
-    if (plugin == nil) {
-        return NO;
-    }
-    
-    return [plugin handleUserActivity:userActivity];
+    return [[Branch getInstance] continueUserActivity:userActivity];
 }
 
 @end
+
